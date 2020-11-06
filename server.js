4dbcf2be730429319c990ca0c92a6c0adb9a53ab
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-11-06 13:54:41 -0600

    deploying

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}.`);
