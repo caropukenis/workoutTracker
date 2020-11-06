5d829455cf11bbfb7fcb6cffcbc94beb95aca85d
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-11-06 14:15:55 -0600

    updates?

--- models/workout.js
+++ models/workout.js
@@ -13,16 +13,16 @@ const workoutSchema = new Schema(
         type: {
           type: String,
           trim: true,
-          required: "Enter and exercise type.",
+          required: "Enter an exercise type",
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter and exercise name.",
+          required: "Enter an exercise name",
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes.",
+          required: "Enter an exercise duration in minutes",
         },
         weight: {
           type: Number,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,32 @@
 const db = require("../models");
 
 module.exports = function (app) {
+  // Used by api.js to getLastWorkout()
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then((workout) => {
         res.json(workout);
       })
       .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  // Creates a new workout in the workout database, createExercise() in api.js
+  app.post("/api/workouts", async (req, res) => {
+    db.Workout.create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
         res.status(400).json(err);
       });
   });
 
+  // Used by api.js to addExercise() to a workout
   app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(req.params.id, {
-      $push: { exercies: req.body },
+      $push: { exercises: req.body },
     })
       .then((dbWorkout) => {
         res.json(dbWorkout);
@@ -23,7 +36,8 @@ module.exports = function (app) {
       });
   });
 
-  app.get("/api/workout/range", (req, res) => {
+  // Used by api.js to getWorkoutsInRange()
+  app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
       .then((workout) => {
         res.json(workout);
